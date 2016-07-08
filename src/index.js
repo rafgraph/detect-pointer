34892fa0b98794b90dd7fe832284d6c0a9b2f13e
rafrex <code@rafrex.com>
2016-07-07 20:26:32 -0700

    Replace this with detectPointer to prevent scope issues

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const detectPointer = {
   update() {
     if (typeof window !== 'undefined' && typeof window.matchMedia === 'function') {
-      this.fine = window.matchMedia('(pointer: fine)').matches;
-      this.coarse = window.matchMedia('(pointer: coarse)').matches;
-      this.none = window.matchMedia('(pointer: none)').matches;
-      this.anyFine = window.matchMedia('(any-pointer: fine)').matches;
-      this.anyCoarse = window.matchMedia('(any-pointer: coarse)').matches;
-      this.anyNone = window.matchMedia('(any-pointer: none)').matches;
+      detectPointer.fine = window.matchMedia('(pointer: fine)').matches;
+      detectPointer.coarse = window.matchMedia('(pointer: coarse)').matches;
+      detectPointer.none = window.matchMedia('(pointer: none)').matches;
+      detectPointer.anyFine = window.matchMedia('(any-pointer: fine)').matches;
+      detectPointer.anyCoarse = window.matchMedia('(any-pointer: coarse)').matches;
+      detectPointer.anyNone = window.matchMedia('(any-pointer: none)').matches;
     }
   },
 };
