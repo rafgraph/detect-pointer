6a1493ad062f77b3fc6cc27139a4561d0bb3f0f9
Thomas <thomas.tilkema@tnt-digital.com>
2017-04-21 11:42:57 +0200

    Added Typescript definition file

---
+++ index.d.ts
@@ -0,0 +1,19 @@
+// Type definitions for detectPointer
+// Project: https://github.com/rafrex/detect-pointer
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.1
+
+declare namespace detectPointer
+{
+  const anyCoarse: boolean;
+  const anyFine: boolean;
+  const anyNone: boolean;
+
+  const coarse: boolean;
+  const fine: boolean;
+  const none: boolean;
+
+  const update: () => void;
+}
+
+export default detectPointer;
